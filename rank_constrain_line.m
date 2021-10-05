ee62121b583510eb1bfafa1ade4d913d2337f548
Amr <eng.amrabualwafa@gmail.com>
2021-10-05 19:12:19 +0200

    05.10.2021

--- rank_constrain_line.m
+++ rank_constrain_line.m
@@ -1,16 +1,14 @@
-x_1 = [3, 2]
-x_2 = [6, 5]
-x_3 = [2, 1]
+l_1 = [3, 2, 0]'
+l_2 = [3, 4, -2]'
+l_3 = [3, 0, 2]'
 T_2 = [0, 2, -2]';
 T_3 = [0, -2, 2]';
-R = eye(3)
+R_2 = eye(3)
+R_3 = eye(3)
 
-x_1 = [x_1, 1]'
-x_2 = [x_2, 1]'
-x_3 = [x_3, 1]'
-M_p = [skew(x_2) * R * x_1, skew(x_2)*T_2;
-       skew(x_3) * R * x_1, skew(x_3)*T_3]
-rank(M_p)
-if (rank(M_p) == 1)
+M_l = [l_2' * R_2 * skew(l_1), l_2' * T_2;
+       l_3' * R_3 * skew(l_1), l_3' * T_2]
+rank(M_l)
+if (rank(M_l) == 1)
     fprintf('\n matching triplet \n')
-end 
+end
--- rank_constrain_point.m
+++ rank_constrain_point.m
@@ -1,14 +1,16 @@
-l_1 = [3, 2, 0]'
-l_2 = [6, 5, 0]'
-l_3 = [2, 1, 0]'
+x_1 = [3, 2]
+x_2 = [6, 5]
+x_3 = [2, 1]
 T_2 = [0, 2, -2]';
 T_3 = [0, -2, 2]';
-R_2 = eye(3)
-R_3 = eye(3)
+R = eye(3)
 
-M_p = [l_2' * R_2 * skew(l_1), l_2' * T_2;
-       l_3' * R_3 * skew(l_1), l_3' * T_2]
-rank(M_l)
-if (rank(M_l) == 1)
+x_1 = [x_1, 1]'
+x_2 = [x_2, 1]'
+x_3 = [x_3, 1]'
+M_p = [skew(x_2) * R * x_1, skew(x_2)*T_2;
+       skew(x_3) * R * x_1, skew(x_3)*T_3]
+rank(M_p)
+if (rank(M_p) == 1)
     fprintf('\n matching triplet \n')
-end
+end 
